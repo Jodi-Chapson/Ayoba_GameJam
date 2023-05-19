a9153944157094cb0409d83b79fb442bcd15b22a
SherilynneHerb <Sherry126chc@gmail.com>
2023-05-20 01:43:53 +0200

    Base Scripts

---
+++ Ayoba-Gamejam/Assets/Scripts/Player/Weapon/WeaponComponent.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class WeaponComponent : MonoBehaviour
+{
+    ProjectileSystem m_projectileSystem;
+    [SerializeField]
+    float m_projectileSpeed = 3;
+
+    private void Start()
+    {
+        m_projectileSystem = ProjectileSystem.GetInstance();
+    }
+
+    public void FireWeapon(Vector3 startPos, Vector3 _target)
+    {
+        m_projectileSystem.FireProjectile(startPos, _target, m_projectileSpeed);
+    }
+}
